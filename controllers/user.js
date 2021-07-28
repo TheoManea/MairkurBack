353eb778cd67b9c6146edb4c300a424240c26f84
moipastoi95 <vincent.ky09@gmail.com>
2021-07-28 15:05:45 +0200

    bug solved i guess

--- controllers/user.js
+++ controllers/user.js
@@ -53,28 +53,3 @@ exports.login = (req, res, next) => {
         });
     });
 };
-
-
-exports.getAssos = (req, res, next) => {
-    // id de l'école sur laquelle recup les infos
-    // check si on a toutes les var
-    if (!req.body.hasOwnProperty('idSchool')) {
-        return res.status(400).send("idSchool is require");
-    }
-
-    idSchool = req.body.idSchool;
-    idSchool = connection.escape(idSchool);
-
-    // connexion à la base
-    connection.getConnection(function (err, connection) {
-        connection.query('SELECT * FROM assostab WHERE idSchool=' + idSchool + ' ORDER BY title ASC', function (error, results, fields) {
-            // gère les erreurs
-            if (error) throw error;
-
-            // renvoie de la réponse
-            res.status(200).json(results);
-            // ferme la co
-            connection.release();
-        });
-    });
-};
