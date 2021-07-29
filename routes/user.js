7074524005ad6db9cd90b2e62eb9f2434d54f0d6
moipastoi95 <vincent.ky09@gmail.com>
2021-07-29 14:25:03 +0200

    bcrypt removed

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
+const e = require('express');
 const express = require('express');
 const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
-//router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
