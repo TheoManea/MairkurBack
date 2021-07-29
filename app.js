38815a38fc34307b6482cdb0c0c4ba5e930fddbf
moipastoi95 <vincent.ky09@gmail.com>
2021-07-29 12:24:53 +0200

    disable bodyparser just to see

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+//const bodyParser = require('body-parser');
 // require('dotenv').config();
 
 const app = express();
