55eb8a875da62fbf5ab27d7d16b3b12c914604c1
moipastoi95 <vincent.ky09@gmail.com>
2021-08-03 17:03:46 +0200

    un jour ça va marcher

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+var bcrypt = require('bcryptjs');
 
 const connection = mysql.createPool({
     host: process.env.DB_HOST,
